cb2262f05dd2cb0c27bea4e7b7025d65dded7792
Abhay Keshri <ishukeshri17@gmail.com>
2026-08-08 11:53:12 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.15%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        while(low<=high){
+             long long mid=low+(high-low)/2;
+             if(1LL * mid * mid==x){
+                return mid;
+             }
+             else if(1LL * mid * mid<x){
+                low=mid+1;
+             }
+             else{
+                high=mid-1;
+             }
+        
+             
+        }
+        return high;
+
+    }
+};
