9c479a85294948b8be322a5e66187aea17fc2300
Abhay Keshri <ishukeshri17@gmail.com>
2026-09-09 12:42:20 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.44%) - LeetHub

--- 4245-count-commas-in-range/4245-count-commas-in-range.cpp
+++ 4245-count-commas-in-range/4245-count-commas-in-range.cpp
@@ -9,6 +9,12 @@ public:
             return commas;
         }
 
+        if(n>1000000){
+            commas=(n-1000000+1)*2;
+        }
+
+        else
+
          commas=n-1000+1;
    
    return commas;
