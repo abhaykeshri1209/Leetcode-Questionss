0c769d74fe4d141d7470dc4db7c7719d243cba1c
Abhay Keshri <ishukeshri17@gmail.com>
2026-09-18 16:48:03 +0530

    Time: 77 ms (5.48%), Space: 76.4 MB (12.61%) - LeetHub

---
+++ 0922-possible-bipartition/0922-possible-bipartition.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+         vector<vector<int>> dislike(n + 1);
+
+     
+        for(auto it : dislikes) {
+            int a = it[0];
+            int b = it[1];
+
+            dislike[a].push_back(b);
+            dislike[b].push_back(a);
+        }
+
+
+
+     vector<int> color(n + 1, -1);
+        for(int i=1;i<=n;i++){
+            if(color[i]==-1){
+                queue<int>q;
+                q.push(i);
+                color[i]=0;
+
+
+                while(!q.empty()){
+                    int node=q.front();
+                    q.pop();
+
+                   for(auto it : dislike[node]){
+                        if(color[it]==-1){
+                            color[it] =!color[node];
+                            q.push(it);
+                        }
+                        else if(color[it]==color[node]){
+                            return false;
+                        }
+                    }
+                }
+            }
+           
+
+        } return true;
+    }
+};
